141e2356a0a3951161b296b51972afcd89307410
diegokim <diego.f.kim@gmail.com>
2019-08-08 23:47:18 -0300

    some refactors

--- src/components/AddExercisePage/AddExercisePage.js
+++ src/components/AddExercisePage/AddExercisePage.js
@@ -3,8 +3,14 @@ import MathTextBox from '../MathTextBox';
 import Button from '@material-ui/core/Button';
 import styles from './AddExercisePage.css'
 import MathText from '../MathText'
+import { withRouter } from 'react-router-dom';
 
 class AddExercisePage extends Component {
+
+    goToHomePage() {
+        let path = `/`;
+        this.props.history.push(path)
+    }
 
     handleExpressionChange({ newExpression }) {
         this.props.handleExpressionChange({ newExpression });
@@ -21,6 +27,8 @@ class AddExercisePage extends Component {
 
     handleAddExercise(event) {
         let { result, expression } = this.props;
+        // TODO: check
+        this.goToHomePage()
         this.props.handleAddExercise({ result, expression });
     }
 
@@ -31,10 +39,12 @@ class AddExercisePage extends Component {
         return (
             <div className="App-content">
                 <div className="add-exercise">
-                
-
                     <div className="form-title">
-                        Nuevo Ejercicio:
+                        Nuevo Ejercicio
+                    </div>
+
+                    <div className="form-title mt-md">
+                        Expresion inicial:
                     </div>
 
                     <div className="math-text-box mt-md">
@@ -49,24 +59,32 @@ class AddExercisePage extends Component {
                     <div className="form-title mt-md">
                         Resultado:
                     </div>
+
                     <div>
                         <MathText content={this.props.result}/>
                     </div>
-                    <div className="math-text-box mt-md">
+
+                    <div className="form-title mt-md">
+                        Si el resultado  no es el esperado puede colocar el correcto aqui:
+                    </div>
+                    
+                    <div className="math-text-box mt-md mb-lg">
                         <MathTextBox
                             content={this.props.result}
                             onContentChange={newResult => this.handleResultChange({ newResult })}
                             >
                         </MathTextBox>
                     </div>
-
+                                
                     <Button className="mt-md" onClick={this.handleGetResult.bind(this)} color="primary">
                         Generar resultado
                     </Button>
 
+                    
                     <Button className="mt-md" onClick={this.handleAddExercise.bind(this)} color="primary">
+                        {/* TODO: chequear que este todo bien */}
                         Agregar Ejercicio
-                </Button>
+                    </Button>
                 </div>
             </div>
 
@@ -74,4 +92,4 @@ class AddExercisePage extends Component {
     }
 }
 
-export default AddExercisePage
+export default withRouter(AddExercisePage)
