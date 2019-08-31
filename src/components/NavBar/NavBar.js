1fdba5c06691ecec05e10c78d0e1b35ecbed210c
diegokim <diego.f.kim@gmail.com>
2019-08-31 19:32:23 -0300

    navbar color and elevation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,51 +6,66 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { withStyles } from '@material-ui/styles';
-import {Link} from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, ThemeProvider } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import clsx from 'clsx';
+import theme from '../../theme'
 
 const useStyles = makeStyles(theme => ({
-    navbar: {
-      flexGrow: 1,
-    },
-    navbarContainer: {
-      display:'flex'
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  navbar: {
+    flexGrow: 1,
+  },
+  navbarContainer: {
+    display: 'flex',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-
-  const styles = {
-    Paper: {
-        colorPrimary: 'red'
+// Sets the color and elevation of the navbar
+const overrideTheme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+        colorPrimary:{
+          backgroundColor: theme.palette.background.dark
+        },
+    },
+    MuiPaper: {
+      elevation4: {
+        boxShadow: 'none'
+      }
     }
-  };
+  },
+});
 
-  export default function NavBar() {
 
-    const classes = useStyles();  
 
-        return (
-          <div className={classes.navbarContainer}>
-            <AppBar position="static" classes={{colorPrimary: 'red'}}>
-                    <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Math Learning
+
+export default function NavBar() {
+
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={overrideTheme}>
+      <div className={classes.navbarContainer}>
+      <AppBar position="static">
+        <Toolbar classes={{ root: { colorPrimary: 'red' } }}>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Math Learning
                     </Typography>
-                    <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </AppBar>
-          </div>
-        )
-    }
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>  
+    </ThemeProvider>
+  )
+}
