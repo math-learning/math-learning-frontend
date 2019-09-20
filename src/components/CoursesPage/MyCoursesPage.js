9a6b94f960b55aac9d677d0d0bb2576478ddf35b
diegokim <diego.f.kim@gmail.com>
2019-09-19 23:33:30 -0300

    UI improvements

--- src/components/CoursesPage/MyCoursesPage.js
+++ src/components/CoursesPage/MyCoursesPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { Container, Typography, Grid, Card } from '@material-ui/core'
 import styles from './MyCoursesPage.module.sass'
+import InteractiveLinkCard from '../InteractiveLinkCard/InteractiveLinkCard'
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
 
 export default class MyCoursesPage extends Component {
   render() {
@@ -8,14 +12,18 @@ export default class MyCoursesPage extends Component {
     return (
       <Container className={styles.defaultContainer} maxWidth="md">
         <Typography variant="h4" className={styles.title}>Mis Cursos</Typography>
-        <Grid container spacing={4}>
+        <Grid container spacing={3}>
           {
             courses.map(course => {
               return (
-                <Grid item xs={12} sm={6} md={4}>
-                  <Card className={styles.courseCard}>
-                    <Typography>{course.name}</Typography>
-                    {
+                <Grid item xs={12}>
+                  <InteractiveLinkCard className={styles.courseCard} path="/pathname">
+                    <Grid container spacing={2}>
+                      <Grid item xs={4}>
+                      <Typography>{course.name}</Typography>
+                      </Grid>
+                      <Grid item xs={5}>
+                      {
                       course.professors.map(professor => {
                         return (
                           <Typography className={styles.professorItem} color="textSecondary" variant="body2" component="p">
@@ -24,7 +32,15 @@ export default class MyCoursesPage extends Component {
                         )
                       })
                     }
-                  </Card>
+                      </Grid>
+                      <Grid item xs={3}>
+                        [100%]
+                      </Grid>
+                    </Grid>
+                    
+                    
+                    
+                  </InteractiveLinkCard>
                 </Grid>
               )
             })
