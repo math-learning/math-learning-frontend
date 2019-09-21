1b9f51460f708d16278ac860a0f50b7563f431d5
diegokim <diego.f.kim@gmail.com>
2019-09-21 12:42:57 -0300

    server url

--- src/configs/variables.js
+++ src/configs/variables.js
@@ -1,6 +1,6 @@
 const configs = {
-    serverUrl: "http://localhost:5000"
-    //serverUrl: "https://math-solver.herokuapp.com"
+    //serverUrl: "http://localhost:5000"
+    serverUrl: "https://math-solver.herokuapp.com"
 };
 
 module.exports = configs;
