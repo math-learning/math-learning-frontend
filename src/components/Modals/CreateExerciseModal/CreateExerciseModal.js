0d72355cb295445be13f32430eaceb7be40f5ad5
LucasLudueno <ludueno.lucas@gmail.com>
2019-11-10 15:44:52 -0300

    adding bootstrap dropdown

--- src/components/Modals/CreateExerciseModal/CreateExerciseModal.js
+++ src/components/Modals/CreateExerciseModal/CreateExerciseModal.js
@@ -1,46 +1,11 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
-import { TextField, Typography, FormControl, InputLabel, Button, Select, InputBase, MenuItem } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { TextField, Typography, FormControl, InputLabel, Button, Select, MenuItem } from '@material-ui/core';
 
 import Modal from '../Modal';
 import MathTextBox from '../../MathTextBox';
+import BootstrapDropdownInput from '../../../bootstrap/dropdownInput';
 import styles from './CreateExerciseModal.module.sass';
-
-const BootstrapInput = withStyles((theme) => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 15,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    }
-  },
-}))(InputBase);
 
 class CreateExerciseModal extends Component {
   constructor(props) {
@@ -153,7 +118,7 @@ class CreateExerciseModal extends Component {
             id="dropdown-selector"
             value={type}
             onChange={this.onChangeType}
-            input={<BootstrapInput />}
+            input={<BootstrapDropdownInput />}
           >
             <MenuItem value="derivada">Derivada</MenuItem>
             <MenuItem value="integral">Integral</MenuItem>
@@ -161,7 +126,7 @@ class CreateExerciseModal extends Component {
         </FormControl>
 
         <div className={styles.exerciseContainer}>
-          <Typography color="textSecondary" variant="h7">
+          <Typography color="textSecondary" variant="body2">
             Escriba el ejercicio
           </Typography>
           <MathTextBox
@@ -178,7 +143,7 @@ class CreateExerciseModal extends Component {
             id="dropdown-selector"
             value={difficulty}
             onChange={this.onChangeDifficulty}
-            input={<BootstrapInput />}
+            input={<BootstrapDropdownInput />}
           >
             <MenuItem value="facil">Fácil</MenuItem>
             <MenuItem value="medio">Medio</MenuItem>
