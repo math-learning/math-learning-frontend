f57f2cd32fd1c61996a4c55c9b8e715ff975478f
Diego Fernando Kim <diego.f.kim@gmail.com>
2020-01-01 18:09:08 -0300

    fix overlapping paths (#25)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Route, Router } from 'react-router-dom';
 
+import { Switch } from 'react-router';
 import AddExercisePage from './components/AddExercisePage';
 import CoursePage from './components/CoursePage';
 import OwnCoursesPage from './components/scenes/courses/OwnCoursesPage';
@@ -62,20 +63,20 @@ const App = () => {
     <main className={classes.root}>
       <Router history={history}>
         <NavBar />
+        <Switch>
+          <Route exact path="/exercises" render={renderExercisesPage} />
+          <Route exact path="/add-exercise" render={renderAddExercisePage} />
+          <Route exact path="/profile" render={renderProfilePage} />
+          <Route path="/my-courses/:courseId/modules/:moduleId" render={renderCoursePage} />
 
-        <Route exact path="/exercises" render={renderExercisesPage} />
-        <Route exact path="/add-exercise" render={renderAddExercisePage} />
-        <Route exact path="/profile" render={renderProfilePage} />
-        <Route path="/my-courses/:courseId/modules/:moduleId" render={renderCoursePage} />
-
-        <Route exact path={configs.paths.main} component={Main} />
-        <Route exact path={configs.paths.courses} component={OwnCoursesPage} />
-        <Route exact path={configs.paths.coursesSearch} component={SearchCoursesPage} />
-        <Route exact path="/courses/:courseId/:guideId/exercises/:exerciseId" component={ExercisePage} />
-        <Route exact path={configs.paths.statistics} render={renderStatisticsPage} />
-        <Route exact path={configs.paths.courseGuide} component={CourseManagement} />
-        <Route exact path={configs.paths.course} component={CourseManagement} />
-
+          <Route exact path={configs.paths.main} component={Main} />
+          <Route exact path={configs.paths.courses} component={OwnCoursesPage} />
+          <Route exact path={configs.paths.coursesSearch} component={SearchCoursesPage} />
+          <Route exact path="/courses/:courseId/:guideId/exercises/:exerciseId" component={ExercisePage} />
+          <Route exact path={configs.paths.statistics} render={renderStatisticsPage} />
+          <Route exact path={configs.paths.courseGuide} component={CourseManagement} />
+          <Route exact path={configs.paths.course} component={CourseManagement} />
+        </Switch>
         <Footer className={classes.footer} />
 
         {/* UTILS */}
