784a02f6deeb95ea7b2c6f0b544c408febe4303c
Lucas Ludueño <ludueno.lucas@gmail.com>
2020-06-13 14:53:37 -0300

    Error count statistic (#63)
    * Changing Exercise error page logic
    
    * Generic statististics

--- src/components/scenes/courses/CoursePage/components/StatisticsPage/ExerciseErrorsPage/ExerciseErrorsPage.js
+++ src/components/scenes/courses/CoursePage/components/StatisticsPage/ExerciseErrorsPage/ExerciseErrorsPage.js
@@ -1,29 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../../../../../../../node_modules/react-vis/dist/style.css';
-import {
-  FlexibleWidthXYPlot, XAxis, YAxis, VerticalBarSeries, RadialChart, LabelSeries
-} from 'react-vis';
-import {
-  CircularProgress, Select, MenuItem, Typography
-} from '@material-ui/core';
-import BootstrapDropdownInput from '../../../../../../../bootstrap/dropdownInput';
+import { CircularProgress } from '@material-ui/core';
 import EmptyStatePage from '../../../../../../common/containers/EmptyStatePage';
+import BaseExerciseStatisticPage from '../BasicExerciseStatisticPage';
 import styles from '../StatisticsCommon.module.sass';
 
-const graphicTypes = ['Histograma', 'Torta'];
-const errorSumTypes = ['Todos', 'Promedio'];
-
-export default class AvgExerciseErrorsPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      guideId: null,
-      errorSumType: 'Todos',
-      graphicType: 'Histograma'
-    };
-  }
-
+export default class AvgExerciseErrorsPage extends BaseExerciseStatisticPage {
   componentDidMount() {
     const { statistics, course, getCourseExerciseErrors } = this.props;
 
@@ -32,68 +14,10 @@ export default class AvgExerciseErrorsPage extends Component {
     }
   }
 
-  calculateCount = (count) => {
-    const { errorSumType } = this.state;
-    const { usersCount } = this.props;
+  onChangeSumType = (event) => {
+    const sumType = event.target.value;
 
-    if (errorSumType === 'Promedio') {
-      return Number((count / usersCount).toFixed(2));
-    }
-    return count;
-  }
-
-  renderGraphic = () => {
-    const { statistics } = this.props;
-    const { graphicType, errorSumType, guideId } = this.state;
-    const currentGuide = guideId || statistics[0].guideId;
-
-    let objsToRender;
-    if (currentGuide === 'Todas') {
-      objsToRender = statistics.map((guide) => {
-        const count = guide.exercises.reduce((acum, ex) => (acum + ex.count), 0);
-        return ({ name: guide.guideId, count });
-      });
-    } else {
-      objsToRender = statistics.find((guide) => guide.guideId === currentGuide).exercises;
-    }
-
-    if (graphicType === 'Histograma') {
-      const data = objsToRender.map((obj) => ({ x: obj.name, y: this.calculateCount(obj.count) }));
-
-      return (
-        <FlexibleWidthXYPlot
-          className={styles.graph}
-          height={400}
-          xType="ordinal"
-          margin={{ bottom: 100 }}
-        >
-          <XAxis tickLabelAngle={-45} />
-          <YAxis title={`${errorSumType === 'Todos' ? 'Cantidad' : 'Promedio'} de errores`} />
-          <VerticalBarSeries data={data} animation barWidth={0.1} />
-          <LabelSeries data={data.map((d) => ({ ...d, label: d.y, xOffset: 15 }))} />
-        </FlexibleWidthXYPlot>
-      );
-    }
-
-    const data = objsToRender.map((obj) => ({
-      label: `${obj.name}: ${this.calculateCount(obj.count)}`,
-      angle: this.calculateCount(obj.count)
-    }));
-
-    return (
-      <RadialChart
-        className={styles.radialGraph}
-        height={400}
-        width={400}
-        data={data}
-        showLabels
-        labelsStyle={{
-          fontSize: 12,
-          letterSpacing: 0.4,
-          fill: 'white'
-        }}
-      />
-    );
+    this.setState({ sumType });
   }
 
   onChangeGuide = (event) => {
@@ -108,31 +32,8 @@ export default class AvgExerciseErrorsPage extends Component {
     this.setState({ graphicType });
   }
 
-  onChangeErrorSumType = (event) => {
-    const errorSumType = event.target.value;
-
-    this.setState({ errorSumType });
-  }
-
-  renderSelector = ({ title, value, onChange, values }) => (
-    <div className={styles.selector}>
-      <Typography className={styles.labelSelector} variant="h6" color="textSecondary">{title}</Typography>
-
-      <Select
-        id={`selector-${title}`}
-        value={value}
-        className={styles.leftSelector}
-        onChange={onChange}
-        input={<BootstrapDropdownInput />}
-      >
-        {values}
-      </Select>
-    </div>
-  );
-
   render() {
     const { statistics } = this.props;
-    const { guideId, graphicType, errorSumType } = this.state;
 
     if (!statistics) {
       return (
@@ -150,34 +51,14 @@ export default class AvgExerciseErrorsPage extends Component {
       );
     }
 
-    const currentGraphic = graphicType;
-    const currentErrorSumType = errorSumType;
-    const currentGuide = guideId || statistics[0].guideId;
-
     return (
       <div className={styles.container}>
         <div className={styles.selectors}>
-          {this.renderSelector({
-            title: 'Guía:',
-            value: currentGuide,
-            onChange: this.onChangeGuide,
-            values: [
-              ...statistics.map((g) => <MenuItem key={g.guideId} value={g.guideId}>{g.guideId}</MenuItem>),
-              <MenuItem key="Todas" value="Todas">Todas</MenuItem>
-            ]
-          })}
-          {this.renderSelector({
-            title: 'Tipo de gráfico:',
-            value: currentGraphic,
-            onChange: this.onChangeGraphicType,
-            values: graphicTypes.map((gt) => (<MenuItem key={gt} value={gt}>{gt}</MenuItem>))
-          })}
-          {this.renderSelector({
-            title: 'Tipo de cuenta:',
-            value: currentErrorSumType,
-            onChange: this.onChangeErrorSumType,
-            values: errorSumTypes.map((gt) => (<MenuItem key={gt} value={gt}>{gt}</MenuItem>))
-          })}
+          {this.renderGuidesSelector()}
+
+          {this.renderGraphicTypeSelector()}
+
+          {this.renderCountTypeSelector()}
         </div>
 
         {this.renderGraphic()}
